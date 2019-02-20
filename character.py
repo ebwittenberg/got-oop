c0b7356f32f1514306ffcddc4222f92e39a66bfa
Eric Wittenberg <ebwittenberg@gmail.com>
2019-02-20 10:48:13 -0500

    added another attribute to the constructor

--- character.py
+++ character.py
@@ -6,10 +6,11 @@
 
 class Character():
     # the "dunder init" method is the constructor
-    def __init__(self, new_name):
+    def __init__(self, new_name, inventory):
         # `self` is the customary way to refer to the instance being built
         # in other languages, they use `this`
         self.name = new_name
+        self.inventory = inventory
 
 
 
